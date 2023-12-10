35fb9007b84bf886f218ed886687b35cce24ac70
Jacob Michels <jacob.michels2025@gmail.com>
2023-12-10 17:41:37 -0500

    complete day 6

---
+++ 6/part1/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualBasic;
+
+namespace part1;
+
+public record Race(int time, int distance);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var races = ParseRaces();
+        var answer = races.Aggregate(1, (product, race) => product * CountWinningStrategies(race));
+        Console.WriteLine(answer);
+    }
+
+    static List<Race> ParseRaces()
+    {
+        string? timeLine = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(timeLine))
+        {
+            throw new InvalidDataException();
+        }
+
+        string? distanceLine = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(distanceLine))
+        {
+            throw new InvalidDataException();
+        }
+
+        var times = timeLine.Split(":")[1].Trim().Split(" ").Where(s => !string.IsNullOrWhiteSpace(s));
+        var distances = distanceLine.Split(":")[1].Trim().Split(" ").Where(s => !string.IsNullOrWhiteSpace(s));
+
+        var zipped = times.Zip(distances)
+        .Select((zip) => new Race(int.Parse(zip.First), int.Parse(zip.Second)));
+
+        return zipped.ToList();
+    }
+
+    static int CountWinningStrategies(Race race)
+    {
+        var count = 0;
+        for (int i = 0; i <= race.time; i++)
+        {
+            var distance = i * (race.time - i);
+            if (distance > race.distance)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
---
+++ 6/part2/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualBasic;
+
+namespace part1;
+
+public record Race(long time, long distance);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var race = ParseRace();
+        var answer = CountWinningStrategies(race);
+        Console.WriteLine(answer);
+    }
+
+    static Race ParseRace()
+    {
+        string? timeLine = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(timeLine))
+        {
+            throw new InvalidDataException();
+        }
+
+        string? distanceLine = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(distanceLine))
+        {
+            throw new InvalidDataException();
+        }
+
+        var timeBroken = timeLine.Split(":")[1].Trim().Split(" ").Where(s => !string.IsNullOrWhiteSpace(s));
+        var distanceBroken = distanceLine.Split(":")[1].Trim().Split(" ").Where(s => !string.IsNullOrWhiteSpace(s));
+
+        var time = long.Parse(string.Join("", timeBroken));
+        var distance = long.Parse(string.Join("", distanceBroken));
+
+        return new Race(time, distance);
+    }
+
+    static int CountWinningStrategies(Race race)
+    {
+        var count = 0;
+        for (int i = 0; i <= race.time; i++)
+        {
+            var distance = i * (race.time - i);
+            if (distance > race.distance)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
