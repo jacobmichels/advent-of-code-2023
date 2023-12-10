1aa91efdbb9735ae47e9a5a9c017216a9483f5a8
Jacob Michels <jacob.michels2025@gmail.com>
2023-12-09 22:19:03 -0500

    add day 4

---
+++ 4/part1/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace part1;
+
+class Card
+{
+    public List<int> Numbers { get; set; }
+    public HashSet<int> Winners { get; set; }
+
+    public Card(List<int> numbers, HashSet<int> winners)
+    {
+        this.Numbers = numbers;
+        this.Winners = winners;
+    }
+
+    public void Print()
+    {
+        Console.Write($"Winners: ");
+        foreach (var winner in Winners)
+        {
+            Console.Write($"{winner} ");
+        }
+        Console.WriteLine();
+
+        Console.Write($"Numbers: ");
+        foreach (var number in Numbers)
+        {
+            Console.Write($"{number} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var cards = ParseCards();
+        foreach (var card in cards)
+        {
+            card.Print();
+        }
+
+        Console.WriteLine($"Score: {CalculateScore(cards)}");
+    }
+
+    static List<Card> ParseCards()
+    {
+        List<Card> cards = new List<Card>();
+        string? line;
+        while ((line = Console.ReadLine()) != null)
+        {
+            var card = ParseCard(line);
+            cards.Add(card);
+        }
+        return cards;
+    }
+
+    static Card ParseCard(string line)
+    {
+        var split = line.Split("|");
+        var numbersSection = split[1].Trim();
+        var winnersSection = split[0].Trim().Split(": ")[1].Trim();
+
+        var winners = winnersSection.Split(" ").Where(num => !string.IsNullOrEmpty(num)).Select(num => int.Parse(num)).ToHashSet();
+        var numbers = numbersSection.Split(" ").Where(num => !string.IsNullOrEmpty(num)).Select(num => int.Parse(num)).ToList();
+
+        return new Card(numbers, winners);
+    }
+
+    static int CalculateScore(List<Card> cards)
+    {
+        var sum = 0;
+        foreach (var card in cards)
+        {
+            sum += CalculateCardScore(card);
+        }
+
+        return sum;
+    }
+
+    static int CalculateCardScore(Card card)
+    {
+        return card.Numbers.Aggregate(0, (sum, num) =>
+        {
+            if (card.Winners.Contains(num))
+            {
+                if (sum == 0)
+                {
+                    sum = 1;
+                }
+                else
+                {
+                    sum = sum * 2;
+                }
+            }
+            return sum;
+        });
+    }
+}
---
+++ 4/part2/Program.cs
@@ -0,0 +1,114 @@
+﻿namespace part1;
+
+class Card
+{
+    public List<int> Numbers { get; set; }
+    public HashSet<int> Winners { get; set; }
+    public int Copies { get; set; }
+    public int Score { get; set; }
+
+    public Card(List<int> numbers, HashSet<int> winners)
+    {
+        this.Numbers = numbers;
+        this.Winners = winners;
+        this.Copies = 1;
+        this.Score = 0;
+    }
+
+    public void Print()
+    {
+        Console.Write($"Winners: ");
+        foreach (var winner in Winners)
+        {
+            Console.Write($"{winner} ");
+        }
+        Console.WriteLine();
+
+        Console.Write($"Numbers: ");
+        foreach (var number in Numbers)
+        {
+            Console.Write($"{number} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var cards = ParseCards();
+        foreach (var card in cards)
+        {
+            card.Print();
+        }
+
+        ProcessCards(cards);
+        Console.WriteLine($"Cards Count: {CountCards(cards)}");
+    }
+
+    static List<Card> ParseCards()
+    {
+        List<Card> cards = new List<Card>();
+        string? line;
+        while ((line = Console.ReadLine()) != null)
+        {
+            var card = ParseCard(line);
+            cards.Add(card);
+        }
+        return cards;
+    }
+
+    static Card ParseCard(string line)
+    {
+        var split = line.Split("|");
+        var numbersSection = split[1].Trim();
+        var winnersSection = split[0].Trim().Split(": ")[1].Trim();
+
+        var winners = winnersSection.Split(" ").Where(num => !string.IsNullOrEmpty(num)).Select(num => int.Parse(num)).ToHashSet();
+        var numbers = numbersSection.Split(" ").Where(num => !string.IsNullOrEmpty(num)).Select(num => int.Parse(num)).ToList();
+
+        return new Card(numbers, winners);
+    }
+
+    static void ProcessCards(List<Card> cards)
+    {
+        CalculateScores(cards);
+        for (int i = 0; i < cards.Count; i++)
+        {
+            var card = cards[i];
+            for (int j = 0; j < card.Copies; j++)
+            {
+                for (int k = i + 1; k <= i + card.Score; k++)
+                {
+                    cards[k].Copies++;
+                }
+            }
+        }
+    }
+
+    static int CountCards(List<Card> cards)
+    {
+        return cards.Aggregate(0, (sum, card) => sum += card.Copies);
+    }
+
+    static void CalculateScores(List<Card> cards)
+    {
+        foreach (var card in cards)
+        {
+            CalculateCardScore(card);
+        }
+    }
+
+    static void CalculateCardScore(Card card)
+    {
+        card.Score = card.Numbers.Aggregate(0, (sum, num) =>
+        {
+            if (card.Winners.Contains(num))
+            {
+                sum += 1;
+            }
+            return sum;
+        });
+    }
+}
